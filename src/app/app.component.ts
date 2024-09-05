19e7b9566206d8a092e51644a8af051df9d299af
omodtung <dothedung131@gmail.com>
2024-09-05 17:04:43 +0700

    setup scss proxy ,angular json and buld raw of ag.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit,inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 
 @Component({
-  selector: 'app-root',
+  selector: 'wac-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FaIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'whatapps-clone-front';
+private faIconLibrary = inject(FaIconLibrary);
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
